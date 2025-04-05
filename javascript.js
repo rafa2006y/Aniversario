5ff3c3cbec9c0925ca1f3f900ae39ce1a23599f9
rafa2006y <rafael20062503@gmail.com>
2025-04-05 16:57:02 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function updateProgressBar(progressValue) {
+  const progressBar = document.getElementById('progressBar');
+  progressBar.style.width = progressValue + '%';
+}
+
+function simulateProgress() {
+  let progress = 0;
+  const interval = setInterval(() => {
+    if (progress >= 100) {
+      window.location.href="pagina1.html";
+    }
+    
+    else {
+      progress++;
+      updateProgressBar(progress);
+    }
+  }, 75);
+}
+
+simulateProgress();
